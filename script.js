f636d7a5d8446154e833659f32053ba680a73e93
Md. Habibur Rahman <rahhabib777@gmail.com>
2024-03-13 05:23:37 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const donateButton = document.querySelector('.donate-button');
+const volunteerButton = document.querySelector('.volunteer-button');
+
+donateButton.addEventListener('click', () => {
+  alert('Thank you for your donation!');
+});
+
+volunteerButton.addEventListener('click', () => {
+  alert('Thank you for your interest in volunteering!');
+});
